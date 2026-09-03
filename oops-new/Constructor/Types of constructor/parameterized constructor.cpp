8db7c5128c699584f6cdecab38ce6a07225b6c20
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2026-09-03 20:02:41 +0530

    Add parameterized constructor to Teacher class
    Implemented a parameterized constructor for the Teacher class to initialize attributes.

---
+++ oops-new/Constructor/Types of constructor/parameterized constructor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Class
+class Teacher{
+
+private:
+        double salary; 
+public:
+
+    //parameterized constructor since,there are parameters inside parenthesis
+    Teacher(string n,string d,string s,double sal){          //n - name, d - dept, s - subject,sal -salary //constructor with 4 parameters
+        name = n;           
+        dept = d;
+        subject = s;
+        salary = sal;
+    }
+
+    string name;
+    string dept;
+    string subject;
+
+    void getInfo(){
+        cout<<"name : "<< name <<endl;
+        cout<<"subject : "<< subject <<endl;
+    }
+};
+
+int main(){
+    
+    Teacher obj1("Shradha","ComputerScience","C++",25000); //pass values in parenthesis (in single line, all values passed in constructor)
+    obj1.getInfo();
+    
+    return 0;
+}
