1d22d49166e6e8a60404ff0c4ec9790d2c9b5077
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2026-09-03 16:38:17 +0530

    Create protected.cpp

---
+++ oops-new/Access Modifiers/protected.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Class
+class Teacher{
+    protected:
+    string name;
+    string dept;
+    string subject;
+    double salary; 
+
+    //methods of class
+    void changeDept(string newDept){
+        dept = newDept; 
+    }
+};
+
+int main(){
+    
+    Teacher obj1;
+    Teacher obj2;
+    Teacher obj3;
+    Teacher obj4;  
+    
+    obj1.name = "Shradha";
+    obj1.subject = "C++";
+    obj1.dept = "Computer Science";
+    obj1.salary = 25000;
+
+    cout<<obj1.name<< endl;
+    return 0;
+}
