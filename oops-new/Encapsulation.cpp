c93ccbcc746999803c692ed2e7449bb5a54b1f7d
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2026-09-03 18:21:01 +0530

    Create Encapsulation.cpp

---
+++ oops-new/Encapsulation.cpp
@@ -0,0 +1,20 @@
+// class Accountant{
+// public:
+//     string accountId;
+//     string username;
+//     double balance;
+//     string password;
+    
+// };
+//hide sensitive info
+class Accountant{
+
+private:
+    double balance;
+    string password;        //data hiding
+
+public:
+    string accountId;
+    string username;
+    
+};
