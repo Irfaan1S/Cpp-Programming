2b8dd1cb3ef8402f6fb6e6981c0e7d9b1a941346
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2026-09-03 17:28:07 +0530

    Create Some members of class as private.cpp

---
+++ oops-new/Access Modifiers/Some members of class as private.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Class
+class Teacher{
+private:
+        double salary; 
+public:
+    string name;
+    string dept;
+    string subject;
+
+
+    //methods of class
+    void changeDept(string newDept){
+        dept = newDept; 
+    }
+};
+
+
+int main(){
+    
+    Teacher obj1;
+    Teacher obj2;
+    Teacher obj3;
+    Teacher obj4;  
+    
+    obj1.name = "Shradha";
+    obj1.subject = "C++";
+    obj1.dept = "Computer Science";
+    obj1.salary = 25000;
+
+    cout<<obj1.name<< endl;
+    return 0;
+}
