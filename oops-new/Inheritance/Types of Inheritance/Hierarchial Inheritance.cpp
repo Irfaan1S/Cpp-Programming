d8af098cb084a21293a8d3c94810c0fb9d573cc2
Irfaan <138297234+Irfaan1S@users.noreply.github.com>
2026-09-04 16:43:11 +0530

    Create Hierarchial Inheritance.cpp

---
+++ oops-new/Inheritance/Types of Inheritance/Hierarchial Inheritance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person{
+public:
+    string name;
+    int age;
+};
+
+class Student : public Person{     
+public:
+    int rollno;
+};
+
+class Teacher : public Person {
+public:
+    string subject;
+};
